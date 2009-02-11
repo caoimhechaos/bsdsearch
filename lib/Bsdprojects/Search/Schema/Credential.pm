8f3ee47bd8ad96133fbe3fb0d09d2576f10b78ff
Tonnerre Lombard <tonnerre@bsdprojects.net>
2009-02-11 16:58:25 +0100

    Implement login credential support so we can sneak into some sites we want to index.

---
+++ lib/Bsdprojects/Search/Schema/Credential.pm
@@ -0,0 +1,44 @@
+package Bsdprojects::Search::Schema::Credential;
+
+use base qw/DBIx::Class/;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Bsdprojects::Search::Schema::Credential - BSD projects search engine remote credentials
+
+=head1 SYNOPSIS
+
+	my $website = $c->model('Bsdprojects::Search::Schema::Credential')->find(1);
+	...
+
+=head1 DESCRIPTION
+
+BSD projects search engine remote credentials
+
+=cut
+
+__PACKAGE__->load_components(qw/Core/);
+__PACKAGE__->table('authinfo');
+__PACKAGE__->add_columns(qw/id url userid password realm userparm passparm/);
+__PACKAGE__->set_primary_key(qw/id/);
+
+=head1 SEE ALSO
+
+L<DBIx::Class>
+
+=head1 AUTHOR
+
+Tonnerre Lombard <C<tonnerre@bsdprojects.net>>
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
